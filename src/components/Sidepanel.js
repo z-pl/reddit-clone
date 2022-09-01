b84549c244336b407ee70737d816d9917ac98999
ahad abdul <ahad.a2703@gmail.com>
2022-09-01 21:05:44 +1000

    added advertisement board

--- src/components/Sidepanel.js
+++ src/components/Sidepanel.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FilterPanel from "./FilterPanel";
 import FeaturedPanel from "./FeaturedPanel";
+import AdvertisementPanel from "./AdvertisementPanel";
 export default function Sidepanel() {
   return (
     <div className="w-3/12 flex flex-col gap-4">
       <FilterPanel />
       <FeaturedPanel />
+      <AdvertisementPanel />
     </div>
   )
 }
