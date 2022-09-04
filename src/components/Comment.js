748c20ae6351b175e8a8caa2dc596ee549d90320
ahad abdul <ahad.a2703@gmail.com>
2022-09-04 23:15:53 +1000

    fixed nav bar nested link error

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,7 +5,7 @@ export default function Comment(props) {
   const newUpvotes = upvotes > 1000 ? Math.round(upvotes /100) / 10 : upvotes;
 
   return (
-    <li key={nanoid()} className="flex flex-col pl-4 bg-gray-300 px-2 py-4 rounded-lg border-l-8 border-zinc-400 w-full">
+    <li key={nanoid()} className="flex flex-col pl-4 bg-gray-100 px-2 py-4 rounded-lg border-l-8 border-zinc-400 w-full">
       <div className="text-xs font-bold">{username}</div>
       <div className="text-sm width-full break-words">{content}</div>
       <div className="text-sm">{newUpvotes}K</div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,25 +2,29 @@ import React from "react"
 import Searchbar from "./Searchbar"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRedditAlien} from '@fortawesome/free-brands-svg-icons'
-import { Link } from "react-router-dom"
-import { useDispatch } from "react-redux"
+import { Link, useLocation } from "react-router-dom"
+import { useDispatch} from "react-redux"
 import { fetchPosts } from "../redux/PostsAPI"
-export default function Navbar() {
 
+export default function Navbar() {
+  const location = useLocation();
   const dispatch = useDispatch();
+
   const handleHeaderClick = () => {
-    dispatch(fetchPosts())
+    if (location.pathname === "/") dispatch(fetchPosts());
   }
 
   return (
-    <Link to ="/">
       <div className="bg-blue-400 p-4 flex justify-between sticky top-0 z-50">
         <div className="flex items-center gap-2 text-white">
-        <FontAwesomeIcon size="2xl" icon={faRedditAlien} />
-        <h2 className="text-2xl font-bold" onClick={handleHeaderClick}><span className="text-black">Simple</span>Reddit</h2>
+        <Link to="/">
+          <div className="flex gap-2">
+            <FontAwesomeIcon size="2xl" icon={faRedditAlien} />
+            <h2 className="text-2xl font-bold" onClick={handleHeaderClick}><span className="text-black">Simple</span>Reddit</h2>
+          </div>
+        </Link>
         </div>
         <Searchbar />
       </div>
-    </Link>
   )
 }
